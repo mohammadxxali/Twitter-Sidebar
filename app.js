5a329792392c10b8cc45342f8be691d3a8dcdb12
mohammadxxali <ali_mohammad@t-online.de>
2023-08-05 21:46:10 +0200

    add js app edits sidebar btns

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ registerPlugin(MorphSVGPlugin)
 let getVar = key => getComputedStyle(document.documentElement).getPropertyValue(key)
 
 const sidebar = document.querySelector('aside')
+
+sidebar.querySelectorAll('button').forEach(button => {
+    button.addEventListener('pointerenter', e => {
+        to(button, {
+            '--c-background': getVar('--c-hover'),
+            '--c-color': getVar('--c-active'),
+            duration: .15
+        })
+    })
+    button.addEventListener('pointermove', e => {
+        to(button, {
+            '--c-background': getVar('--c-hover'),
+            '--c-color': getVar('--c-active'),
+            duration: .15
+        })
+    })
+    button.addEventListener('pointerleave', e => {
+        to(button, {
+            '--c-background': getVar('--c-sidebar'),
+            '--c-color': button.classList.contains('active') || sidebar.animating === button ? getVar('--c-active') : getVar('--c-default'),
+            duration: .15
+        })
+    })
+})
